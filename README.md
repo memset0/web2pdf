ee14a5fc1fc0096833c1738011a79ad3e32af4ea
memset0 <memset0@outlook.com>
2024-08-05 19:49:58 +0800

    feat: init vite

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# print-to-pdf
+# web2pdf
+
+## Usage
+
+## Dev
+
+> For anyone who wishes to develop this script on [zhihu.com](https://zhihu.com) and some other websites, some means to disable [Content Security Policy (CSP)](https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP) may be required. For Chrome/Edge browsers, you can resolve this issue by installing the [Disable CSP](https://chromewebstore.google.com/detail/disable-content-security/ieelmcmcagommplceebfedjlakkhpden) plugin.
---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite';
+import monkey from 'vite-plugin-monkey';
+
+export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
+  plugins: [
+    monkey({
+      entry: 'src/main.js',
+      userscript: {
+        // icon: 'https://vitejs.dev/logo.svg',
+        namespace: 'memset0/web2pdf',
+        match: [
+          'zhihu.com', //
+          'zhuanlan.zhihu.com',
+          'blog.csdn.net',
+        ]
+          .map((hostname) => [
+            `http://${hostname}`, //
+            `https://${hostname}`,
+            `http://${hostname}/*`,
+            `https://${hostname}/*`,
+          ])
+          .flat(),
+      },
+      build: {
+        externalGlobals: {},
+      },
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
